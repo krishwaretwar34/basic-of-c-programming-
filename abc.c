252c8035a2355a6113ca0ed6b2d23bfd8b060bc6
krishwaretwar34 <waretwark@gmail.com>
2024-09-30 22:12:05 +0530

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int a, b, c;
+    printf("Enter First Number:");
+    scanf("%d",&a);
+
+    printf("Enter Second Number:");
+    scanf("%d",& b);
+
+    c = a+b;
+    printf("Your Addition is %d",&c);
+
+    return 0;
+}
